88783c60c3240b06d3d1d8ab344985beb2be3199
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-16 14:01:32 -0700

    add test for checkPlanetAge in Mercury years. Red phase pass.

--- _test_/calculator.test.js
+++ _test_/calculator.test.js
@@ -8,14 +8,16 @@ describe('User', () => {
   });
 
   test('should correctly create a SpaceTraveller object with name value', () => {
-  expect (traveller.userName).toEqual("name");
+  expect(traveller.userName).toEqual("name");
   });
-  test('should correctly create a calculator object with age value', () => {
-    expect (traveller.age).toEqual(30);
+  test('should correctly create a SpaceTraveller object with age value', () => {
+    expect(traveller.age).toEqual(30);
   });
-  test('should correctly create a calculator object with yearOfBirth value', () => {
-    expect (traveller.yearOfBirth).toEqual(1991);
+  test('should correctly create a SpaceTraveller object with yearOfBirth value', () => {
+    expect(traveller.yearOfBirth).toEqual(1991);
   });
-  test
+  test('should correctly take age value and return age in Mercury years', () => {
+    expect(traveller.checkPlanetAge()).toEqual(125);
+  });
 
 });
