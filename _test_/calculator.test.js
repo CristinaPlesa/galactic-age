bad238738cb02f1d4c59edc8727f31390610fd6e
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-16 11:42:42 -0700

    swrite function for first test, test passed for good fail.

--- _test_/calculator.test.js
+++ _test_/calculator.test.js
@@ -3,9 +3,9 @@ import Calculator from './../src/js/calculator';
 describe('Calculator', () => {
 
   test('should correctly create a calculator object with name and age values', () => {
-  const calculator = Calculator("name",25);
+  const calculator = new Calculator("name",25);
   expect (calculator.userName).toEqual("name");
   expect (calculator.age).toEqual(25);
   });
-  
+
 });
